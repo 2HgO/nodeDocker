681815cfb8c471cdaac86ac0471653162899b176
Oghogho Odemwingie <oghogho@softcom.ng>
2019-09-23 10:46:59 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Practicing dockerized express application
---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const config = {
+	name: 'practice',
+	port: 3000,
+	host: '0.0.0.0',
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+	message: 'success',
+	data: 'your dad',
+	});
+});
+
+app.listen(config.port, config.host, e => {
+	if (e) {
+		throw new Error('Internal Server Error');
+	}
+	console.log(`Server started alright on port ${config.port}`);
+});
